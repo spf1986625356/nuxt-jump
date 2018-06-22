0bb7de1c02d5887150c3383acb19db9c3df5faf4
rebellious <18137611936@163.com>
2018-06-22 10:14:20 +0800

    带请求处理的例子

---
+++ components/hader.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+        <div style="text-align: center;margin-top: 100px;">
+            <h3>
+                <b style="color: aqua;">hello</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <i style="color: chartreuse;">word</i>
+            </h3>
+            <!--You may need an appropriate loader to handle this file type.-->
+            <!--你可能需要一个适当的加载程序来处理这个文件类型。-->
+            <!--<img src="../static/favicon.ico" alt="彩色">-->
+        </div>
+
+    </header>
+</template>
